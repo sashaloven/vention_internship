f52996585cc2455067203e613607768bdbdde703
sashaloven <mps3556335@gmail.com>
2025-05-23 14:52:15 +0200

    add ec2 module

--- modules/VPC/outputs.tf
+++ modules/VPC/outputs.tf
@@ -9,3 +9,12 @@ output "egress_rules_obj" {
 output "ingress_rules_obj" {
     value = local.ingress_rules_obj
 }
+
+output "subnet_ids" {
+  value = { for k, subnet in aws_subnet.this : k => subnet.id }
+}
+
+output "security_group_ids" {
+  value = { for k, sg in aws_security_group.this : k => sg.id }
+}
+
